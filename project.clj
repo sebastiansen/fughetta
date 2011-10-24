b6a02d880ab2f89d9997f84a686dc9d906c60201
Sebastián Rojas <www.sebastiansen@hotmail.com>
2011-10-24 04:12:10 -0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject fughetta "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-				 [jfugue "4.0.3"]])
+	         [jfugue "4.0.3"]])
