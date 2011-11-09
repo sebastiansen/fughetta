fe34627fc4e7379f67a50f7abd7e5cf88987e338
Sebastian Rojas <Sebastian@Sebastian-PC.(none)>
2011-11-09 00:10:33 -0300

    version 0.0.1!

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fughetta "0.0.1-SNAPSHOT"
-  :description "A JFugue nano wrapper for Clojure"
+  :description "A JFugue wrapper for Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
 	         [jfugue "4.0.3"]])
